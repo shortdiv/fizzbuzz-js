c8306c9e8de24d15554dbfa7511babd2f10d17dd
Div <divya@heysparkbox.com>
2014-05-14 16:39:05 -0400

    Added test for buzz

--- spec/numberSpec.js
+++ spec/numberSpec.js
@@ -4,4 +4,8 @@ describe("Number", function(){
     expect(is_fizzbuzz(45)).toBe(true);
   });
 
+  it("should check if a number is divisible by 5", function(){
+    expect(is_buzz(10)).toBe(true);
+  });
+
 });
