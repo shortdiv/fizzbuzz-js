f21ca9d1b002571167e0c1d1e2b678effdcbb444
Div <divya@heysparkbox.com>
2014-05-14 11:31:45 -0400

    First commit

---
+++ spec/numberSpec.js
@@ -0,0 +1,7 @@
+describe("Number", function(){
+  it ("should return 100", function(){
+    var hundreds = new Number();
+    expect(hundreds.count()).toBe(100);
+
+  })
+});
---
+++ src/number.js
@@ -0,0 +1,5 @@
+function Number(){};
+
+Number.prototype.count = function(){
+  return 100;
+}
