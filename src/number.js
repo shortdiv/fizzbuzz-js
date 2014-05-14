8e08b7df017ae1fbaecee3c232a47fab358e2027
Div <divya@heysparkbox.com>
2014-05-14 15:18:37 -0400

    Passed test for buzz

--- src/number.js
+++ src/number.js
@@ -5,6 +5,8 @@ Number.prototype.count = function(){
   for(var i=1; i<=100;i++){
     if (i % 15 === 0){
       array.push("FizzBuzz");
+    } else if(i % 5 === 0){
+      array.push("Buzz");
     } else {
       array.push(i);
     };
