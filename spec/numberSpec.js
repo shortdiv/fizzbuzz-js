c818bb8aec178dce5945df8b67e9ae3e0c9c92e2
Div <divya@heysparkbox.com>
2014-05-14 16:41:11 -0400

    Added test for fizz

--- spec/numberSpec.js
+++ spec/numberSpec.js
@@ -1,11 +1,15 @@
 describe("Number", function(){
 
   it("should check if a number is divisible by 15", function(){
-    expect(is_fizzbuzz(45)).toBe(true);
+    expect(is_fizzbuzz(45)).toEqual(true);
   });
 
   it("should check if a number is divisible by 5", function(){
-    expect(is_buzz(10)).toBe(true);
+    expect(is_buzz(10)).toEqual(true);
+  });
+
+  it("should check if a number is divisible by 3", function(){
+    expect(is_fizz(6)).toEqual(true)
   });
 
 });
