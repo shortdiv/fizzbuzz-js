9dfea1c41ae764e93e34b97e675102bd73d465b7
Div <divya@heysparkbox.com>
2014-05-14 15:16:35 -0400

    Added test for buzz

--- spec/numberSpec.js
+++ spec/numberSpec.js
@@ -1,8 +1,13 @@
 describe("Number", function(){
   it ("should return fizzbuzz for the 15th number", function(){
     var hundreds = new Number();
-    var array = hundreds.count()
+    var array = hundreds.count();
     expect(array[14]).toBe("FizzBuzz");
+  });
 
-  })
+  it("should return buzz for multiples of 5", function(){
+    var number = new Number();
+    var array = number.count();
+    expect(array[4]).toBe("Buzz");
+  });
 });
