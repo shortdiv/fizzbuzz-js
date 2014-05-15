5f3af83abfb27b32dda6d1bf4c39cadf33750b9a
Div <divya@heysparkbox.com>
2014-05-15 10:16:16 -0400

    Added more tests for fizzbuzz

--- spec/numberSpec.js
+++ spec/numberSpec.js
@@ -1,15 +1,50 @@
-describe("Number", function(){
+describe("FIZZBUZZ", function() {
 
-  it("should check if a number is divisible by 15", function(){
-    expect(is_fizzbuzz(45)).toEqual(true);
+  describe("Which Fizzbuzz", function(){
+
+    it("should return fizzbuzz for a multiple of 15", function(){
+      expect(which_fizzbuzz(45)).toEqual("fizzbuzz")
+    });
+
+    it("should return buzz for multiple of 5", function(){
+      expect(which_fizzbuzz(20)).toEqual("buzz");
+    });
+
+    it("should return fizz for multiple of 3", function(){
+      expect(which_fizzbuzz(6)).toEqual("fizz");
+    });
+
+    it("should return fizzbuzz for multiple of 15 and 5", function(){
+      expect(which_fizzbuzz(75)).toEqual("fizzbuzz");
+    });
+
+    it("should return counter for non multiple of 3,5 and 15", function(){
+      expect(which_fizzbuzz(23)).toEqual(23);
+    });
+
   });
 
-  it("should check if a number is divisible by 5", function(){
-    expect(is_buzz(10)).toEqual(true);
-  });
+  describe("fizzbuzz()", function() {
 
-  it("should check if a number is divisible by 3", function(){
-    expect(is_fizz(6)).toEqual(true)
+    beforeEach(function() {
+      spyOn(window, 'which_fizzbuzz');
+    });
+
+   it("should count from 0 to 100 as a default", function(){
+      fizzbuzz();
+      expect(window.which_fizzbuzz.calls.count()).toEqual(100);
+    });
+
+    it("should count from 0 to 100", function(){
+      fizzbuzz(100);
+      expect(window.which_fizzbuzz.calls.count()).toEqual(100);
+    });
+
+    it("should count from 0 to 1000", function(){
+      fizzbuzz(1000);
+      expect(window.which_fizzbuzz.calls.count()).toEqual(1000);
+    });
+
   });
 
 });
--- src/number.js
+++ src/number.js
@@ -1,11 +1,31 @@
 function is_fizzbuzz(counter){
   return counter % 15 === 0;
-}
+};
 
 function is_buzz(counter){
   return counter % 5 === 0;
-}
+};
 
 function is_fizz(counter){
   return counter % 3 === 0;
-}
+};
+
+function which_fizzbuzz(counter){
+  if (is_fizzbuzz(counter)){
+    return('fizzbuzz');
+  } else if (is_buzz(counter)){
+    return('buzz');
+  } else if(is_fizz(counter)){
+    return('fizz');
+  } else{
+    return(counter);
+  };
+};
+
+function fizzbuzz(count){
+  count = count || 100;
+
+  for (var counter = 1; counter <= count; counter++){
+    console.log(which_fizzbuzz(counter));
+  };
+};
